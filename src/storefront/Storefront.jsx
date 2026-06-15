e1e116176d22ed9af4a06a4165c430b0f1f14d43
Maged <magedabuzied95@gmail.com>
2026-06-15 03:35:44 +0300

    revert unsafe storefront shipping label sanitization

--- src/storefront/Storefront.jsx
+++ src/storefront/Storefront.jsx
@@ -1410,7 +1410,7 @@ const normalizeStorefrontPaymentSettings = (settings = {}) => {
     shippingConfirmation: {
       enabled: bool(settings["storefront.payment_methods.shipping_confirmation_enabled"], true),
       amount: number(settings["storefront.payment_methods.shipping_confirmation_amount"], 75),
-      label: repairArabicMojibakeText(text(settings["storefront.payment_methods.shipping_confirmation_label"], sfText("storefront.checkout.transfer.amountDueNow"))),
+      label: sfText("storefront.checkout.transfer.amountDueNow"),
     },
   };
 };
